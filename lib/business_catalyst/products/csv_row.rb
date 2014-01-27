fc27a9c779e74fb1bc4a4fb6cf1833f8f3205705
William Makley <wmakley@tortus.com>
2014-01-27 10:20:17 -0500

    remove transform_enabled because it is now redundant with behavior applied to all booleans

--- lib/business_catalyst/products/csv_row.rb
+++ lib/business_catalyst/products/csv_row.rb
@@ -172,11 +172,6 @@ module BusinessCatalyst
           end
         end
 
-        # Make sure enabled is false if input is nil
-        def transform_enabled(input)
-          input ? "Y" : "N"
-        end
-
     end
   end
 end
