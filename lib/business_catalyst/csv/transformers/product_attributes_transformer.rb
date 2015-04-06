079d291990e63ecc5ac909ba1f24527e765bb17c
William Makley <wmakley@tortus.com>
2015-04-06 15:43:37 -0400

    allow specifing display_as as a symbol

--- lib/business_catalyst/csv/transformers/product_attributes_transformer.rb
+++ lib/business_catalyst/csv/transformers/product_attributes_transformer.rb
@@ -2,9 +2,17 @@
 module BusinessCatalyst
   module CSV
 
-    Attribute = Struct.new(:name, :display_as, :required, :keep_stock) do
-
+    ProductAttribute = Struct.new(:name, :display_as, :required, :keep_stock) do
       Option = Struct.new(:name, :image, :price)
+
+      def initialize(name, display_as = nil, required = false, keep_stock = false)
+        display_as = normalize_display_as(display_as)
+        super(name, display_as, required, keep_stock)
+      end
+
+      def display_as=(value)
+        super(normalize_display_as(value))
+      end
 
       def options
         @options ||= []
@@ -16,6 +24,27 @@ module BusinessCatalyst
         option
       end
 
+      private
+
+      def normalize_display_as(display_as)
+        if display_as.kind_of?(Symbol)
+          case display_as
+          when :dropdown
+            5
+          when :checkbox
+            6
+          when :radio
+            7
+          else
+            raise ArgumentError, "display_as must be :dropdown, :checkbox, or :radio"
+          end
+        elsif ![5, 6, 7].include?(display_as)
+          raise ArgumentError, "display_as must be 5, 6, or 7"
+        else
+          display_as
+        end
+      end
+
     end
 
     # Example: Chain*|5|N:Rope Chain||US/0,Box Chain||US/5,Snake Chain||US/5;Length*|5|N:16 inch||US/0,18 inch||US/0,20 inch||US/0,24 inch||US/0
@@ -23,19 +52,11 @@ module BusinessCatalyst
     # Name1REQ|display_as|keep_stock:Option1.name|Option1.image|Option1.price,Option2.name|...;Name2...
     class ProductAttributesTransformer < Transformer
 
-      def attributes
-        if input.kind_of?(Array)
-          input
-        else
-          [input]
-        end
-      end
-
       def transform
         attributes.map {|attribute|
           name        = attribute.name
           required    = attribute.required ? '*' : ''
-          display_as  = attribute.display_as
+          display_as  = convert_display_as(attribute.display_as)
           keep_stock  = BooleanTransformer.transform(attribute.keep_stock)
 
           text = ["#{name}#{required}", display_as, keep_stock].join("|")
@@ -47,6 +68,27 @@ module BusinessCatalyst
 
           text
         }.join(";")
+      end
+
+      def attributes
+        if input.kind_of?(Array)
+          input
+        else
+          [input]
+        end
+      end
+
+      def convert_display_as(value)
+        case value
+        when :dropdown
+          5
+        when :checkbox
+          6
+        when :radio
+          7
+        else
+          value.to_i
+        end
       end
 
     end
--- spec/lib/business_catalyst/csv/transformers/product_attributes_transformer_spec.rb
+++ spec/lib/business_catalyst/csv/transformers/product_attributes_transformer_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 
 module BusinessCatalyst
   module CSV
+    describe ProductAttribute do
+      it "raises ArgumentError when display_as is invalid symbol" do
+        expect {
+          ProductAttribute.new("Test", :invalid, false, false)
+        }.to raise_error(ArgumentError)
+      end
+      it "raises ArgumentError when display_as is invalid integer" do
+        expect {
+          ProductAttribute.new("Test", -1, false, false)
+        }.to raise_error(ArgumentError)
+      end
+    end
+
     # Example: Chain*|5|N:Rope Chain||US/0,Box Chain||US/5,Snake Chain||US/5;Length*|5|N:16 inch||US/0,18 inch||US/0,20 inch||US/0,24 inch||US/0
     describe ProductAttributesTransformer do
       subject { ProductAttributesTransformer }
@@ -12,35 +25,35 @@ module BusinessCatalyst
       end
 
       it "turns Attribute object graph into BC string" do
-        chain = Attribute.new("Chain", 5, false, false)
+        chain = ProductAttribute.new("Chain", 5, false, false)
         chain.add_option("Rope Chain", "image1", 5)
         expect(subject.transform(chain)).to eq("Chain|5|N:Rope Chain|image1|US/5")
       end
 
       it "appends '*' to required attribute names" do
-        chain = Attribute.new("Chain", 5, true, false)
+        chain = ProductAttribute.new("Chain", 5, true, false)
         chain.add_option("Rope Chain", "image1", 5)
         expect(subject.transform(chain)).to eq("Chain*|5|N:Rope Chain|image1|US/5")
       end
 
       it "joins multiple options with ','" do
-        chain = Attribute.new("Chain", 5, false, false)
+        chain = ProductAttribute.new("Chain", 5, false, false)
         chain.add_option("Rope Chain", "image1", 5)
         chain.add_option("Snake Chain", "image2", 5)
         expect(subject.transform(chain)).to eq("Chain|5|N:Rope Chain|image1|US/5,Snake Chain|image2|US/5")
       end
 
       it "joins multiple attributes with ';'" do
-        chain = Attribute.new("Chain", 5, false, false)
+        chain = ProductAttribute.new("Chain", 5, false, false)
         chain.add_option("Rope Chain", "image1", 5)
-        length = Attribute.new("Length", 5, true, true)
+        length = ProductAttribute.new("Length", 5, true, true)
         length.add_option("16 inch", "image2", 10)
         attributes = [chain, length]
         expect(subject.transform(attributes)).to eq("Chain|5|N:Rope Chain|image1|US/5;Length*|5|Y:16 inch|image2|US/10")
       end
 
       it "converts display_as value of :dropdown to 5" do
-        chain = Attribute.new("Chain", :dropdown, false, false)
+        chain = ProductAttribute.new("Chain", :dropdown, false, false)
         chain.add_option("Rope Chain", "image1", 5)
         expect(subject.transform(chain)).to eq("Chain|5|N:Rope Chain|image1|US/5")
       end
